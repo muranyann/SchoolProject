caa18da3b68b41d00c01516a4a5080bb7502c6a8
muranyann <murany_ann@gmx.com>
2025-03-06 16:07:24 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SchoolProject {
+  public static void main(String[] args) {
+    // Your code goes here
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter your name: ");
+    String name = scanner.nextLine();
+    System.out.println("Hello, " + name + "!");
+  }
+}
